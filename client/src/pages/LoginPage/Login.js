a0e77c0228f0057ba347909ca289c2cb36ee7f28
DuyThanh2207 <69976700+DuyThanh2207@users.noreply.github.com>
2020-10-13 00:38:50 +0700

    Update Login.js

--- client/src/pages/LoginPage/Login.js
+++ client/src/pages/LoginPage/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import logo from './Logo.jpg'
 const Login = () => {
     const { register, handleSubmit, errors } = useForm();
     let history = useHistory();
